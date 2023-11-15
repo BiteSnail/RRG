1801afe6caf27142cb19cfd70f564ebaa1ecbad4
Jiwon Choi <sjndlw1013@gmail.com>
2023-11-15 22:55:44 +0900

    스테이지1 진행 오브젝트 생성

--- client/RRG/Assets/Resources/Scripts/Contents/StageBase.cs
+++ client/RRG/Assets/Resources/Scripts/Contents/StageBase.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class StageBase : MonoBehaviour
 {
-    protected int bpm = 60; //설정 필요
+    public int bpm = 60; //설정 필요
     protected double currentTime = 0;
-    protected double exceedRange = 0.3f;
+    public double exceedRange = 0.3f;
 
-    protected int[] hitBeatNums; //스테이지별로 작성 필요
+    public int[] hitBeatNums; //스테이지별로 작성 필요
     protected bool[] isHitBeat = new bool[200];
     protected int nowBeatIndex = 0;
 
