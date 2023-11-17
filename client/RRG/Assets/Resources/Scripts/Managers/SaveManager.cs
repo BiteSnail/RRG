dc7c48bb75bf2c6ba00dc3463b63678bd9e200e0
bitesnail <rkwoal216@gmail.com>
2023-11-17 23:30:27 +0900

    SCRUM-49 Save와 SaveManager 구현

---
+++ client/RRG/Assets/Resources/Scripts/Managers/SaveManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveManager
+{
+    private List<Save> saves;
+    private Save currentSave;
+    
+    void Start()
+    {
+        LoadSaves();
+    }
+
+    private LoadSaves()
+    {
+        this.saves = new List<Save>();
+        //파일에서 불러오는 것도 구상 중
+    }
+
+    public void startRecording(StageBase stage)
+    {
+        this.currentSave = new Save(stage);
+    }
+
+    public void wrong(Item item)
+    {
+        currentSave.addWrong(item);
+    }
+
+    public void correct(Item item)
+    {
+        currentSave.addCorrect(item);
+    }
+
+    public string report()
+    {
+        saves.Add(currentSave);
+        currentSave = null;
+        return string.Format("wrongs: %d \ncorrect: %d", currentSave.getWrongScore(), currentSave.getCorrectScore());
+    }
+}
