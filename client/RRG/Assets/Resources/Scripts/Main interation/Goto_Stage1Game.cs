bf1985f3a6aaeacdece27fe6ffebe756c03028c8
Jiwon Choi <sjndlw1013@gmail.com>
2023-11-23 21:16:42 +0900

    Stage1 오프닝 씬 이동 추가

---
+++ client/RRG/Assets/Resources/Scripts/Main interation/Goto_Stage1Game.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goto_Stage1Game : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(StartGame());
+    }
+
+    IEnumerator StartGame()
+    {
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene("Stage1"); //Stage1 ?¨ÏúºÎ°??¥Îèô
+    }
+}
---
+++ client/RRG/Assets/Resources/Scripts/Main interation/Goto_Stage1Opening.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // ?„ìš”
+
+public class Goto_Stage1Opening : MonoBehaviour
+{
+
+    public void GameScnesCtrl()
+    {
+        SceneManager.LoadScene("Stage1_Opening"); //Stage1 ?¬ìœ¼ë¡??´ë™
+    }
+
+}
