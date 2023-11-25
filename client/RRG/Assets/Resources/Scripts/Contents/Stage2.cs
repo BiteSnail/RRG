2ae03ed7e5b62613be5002919a6954ec4944fbc6
Jiwon Choi <sjndlw1013@gmail.com>
2023-11-25 22:38:31 +0900

    Stage2 버튼 분기 틀 작성

--- client/RRG/Assets/Resources/Scripts/Contents/Stage2.cs
+++ client/RRG/Assets/Resources/Scripts/Contents/Stage2.cs
@@ -11,8 +11,59 @@ public class Stage2 : StageBase
     [SerializeField] float itemSpawnY;
     [SerializeField] float hitY;
 
+    Vector3 targetPos;
+    int pressed = 0;
+
     private void Update()
     {
+        currentTime += Time.deltaTime;
+
+        if (item == null)
+            targetPos = Vector3.zero;
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && pressed < 2 ) //특수키
+        {
+            if (IsCorrectHit())
+            {
+                if (item.type == ItemType.General)
+                {
+                    //맞음 
+                    targetPos = generalPos.transform.position;
+                    Managers.Sound.Play("General");
+
+                }
+                else if (item.type == ItemType.Mixed)
+                {
+                    if (item.pair_first.type == ItemType.General
+                        || item.pair_second.type == ItemType.General)
+                    {
+                        if(pressed == 1) //맞음(최종) 
+                        {
+                            //쓰레기 종류에 따라 분리된 쓰레기를 쓰레기통에 보냄
+                            MoveItemtoBox(item.pair_first);
+                            MoveItemtoBox(item.pair_second);
+                        }
+                    }
+                    else //박자는 맞았는데 분류가 틀림
+                    {
+                        Managers.Sound.Play("Fail");
+                    }
+                }
+                else //박자는 맞았는데 분류가 틀림
+                {
+                    Managers.Sound.Play("Fail");
+                }
+            }
+            else //쓰레기를 놓침
+            {
+
+            }
+            pressed++;
+        }
+   
+
+
         if (currentTime >= 60d / bpm) //매 박자마다
         {
             nowBeatIndex++;
@@ -21,6 +72,7 @@ public class Stage2 : StageBase
             if (isHitBeat[nowBeatIndex] == true)
             {
                 ItemSpawn();
+                pressed = 0;
                 Managers.Sound.Play("ItemSpawn");
             }
             //if (targetPos == itemGotoPos.transform.position)
@@ -44,5 +96,35 @@ public class Stage2 : StageBase
     void DropItem()
     {
         item.transform.position = new Vector2(item.transform.position.x, transform.position.y + 1);
+    }
+
+    void MoveItemtoBox(Item tem)
+    {
+        if(tem.type == ItemType.General)
+        {
+            targetPos = generalPos.transform.position;
+        }
+        else if(tem.type == ItemType.Paper)
+        {
+            targetPos = paperPos.transform.position;
+        }
+        else if(item.type == ItemType.Can)
+        {
+            targetPos = canPos.transform.position;
+        }
+        else if(item.type == ItemType.Glass)
+        {
+            targetPos = glassPos.transform.position;    
+        }
+        else if (item.type == ItemType.Plastic)
+        {
+            targetPos = plasticPos.transform.position;  
+        }
+
+    }
+
+    bool IsCorrectHit()
+    {
+        return true;
     }
 }
