50a7210330fcc9730f1b41deb531e9b431a482e6
zizioz <hi27778@gmail.com>
2023-11-28 21:42:35 +0900

    MenuSet 스크립트 수정

--- client/RRG/Assets/Resources/Scripts/Contents/MenuSet.cs
+++ client/RRG/Assets/Resources/Scripts/Contents/MenuSet.cs
@@ -5,16 +5,6 @@ public class MenuSet : MonoBehaviour
     public static bool GameIsPaused = false; // 게임 정지
     public GameObject PauseMenu;
 
-    // SoundManager instance 참조
-    private SoundManager soundManager;
-
-    void Start()
-    {
-        // SoundManager 초기화
-        soundManager = new SoundManager();
-        soundManager.Init();
-    }
-
     // Esc 누르면 Exit 버튼 창, 한번 더 누르면 다시 시작 
     void Update()
     {
@@ -38,8 +28,7 @@ public class MenuSet : MonoBehaviour
         Time.timeScale = 1f;
         GameIsPaused = false;
 
-        // 해결 필요
-        soundManager.UnPauseBGM();
+        Managers.Sound.UnPauseBGM();
     }
 
     public void Pause()
@@ -49,7 +38,6 @@ public class MenuSet : MonoBehaviour
         Time.timeScale = 0f;
         GameIsPaused = true;
 
-        // 해결 필요
-        soundManager.PauseBGM();
+        Managers.Sound.PauseBGM();
     }
 }
